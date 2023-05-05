0d48104ac6489c239a3abdf0e0ff5b6558b48c54
ravi teja <ravit2839@gmail.com>
2023-05-04 20:57:40 -0700

    added app selector

---
+++ frontend/selectors/appSelector.js
@@ -0,0 +1,7 @@
+export const selectAlertFlag = (state) => state.app.alert;
+export const selectToastFlag = (state) => state.app.toast;
+export const selectAlertMessage = (state) => state.app.alertMessage;
+export const selectAlertType = (state) => state.app.alertType;
+export const selectIsLoggedIn = (state) => state.app.isLoggedIn;
+export const selectUser = (state) => state.app.user;
+export const selectCountries = (state) => state.app.countries;
